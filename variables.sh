0554140373c80dbc656e2c0eefaa0d1ffca348fe
Antonio Nunez <antonio@altresear.ch>
2026-02-16 13:58:41 +0800

    feat: update to v3.9.5

--- variables.sh
+++ variables.sh
@@ -1,10 +1,10 @@
 set -e
 FN_CONTAINER_NAME=orbit-full-node
 DOCKER_REPO=offchainlabs/nitro-node
-DOCKER_TAG=v3.6.7-a7c9f1e
+DOCKER_TAG=v3.9.5-66e42c4
 PORT_RPC=8547
 PORT_WS=8548
 PERSISTENCE=persistence
 RPC_URL=
-EXTRA_OPTS=
+EXTRA_OPTS="--validation.wasm.allowed-wasm-module-roots=/home/user/nitro-legacy/machines --validation.wasm.root-path=/home/user/nitro-legacy/machines"
 set +e
